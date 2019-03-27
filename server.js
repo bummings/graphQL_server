e625637988cb267359809b09a573f0edf250fe3c
bummings <eabummings@gmail.com>
2019-03-26 23:58:49 -0400

    dresses express server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema'):
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}));
+
+const PORT = process.env.PORT || 5555;
+
+app.listen(PORT, () => console.log(`Listenin on port ${PORT}`));
