f6759b031fb6cb49b0e359df032b1e220a631f19
bummings <eabummings@gmail.com>
2019-03-28 22:40:34 -0400

    dummy data in lieu of server

--- schema.js
+++ schema.js
@@ -7,6 +7,13 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
+//hardcoded data in lieu of server
+const customers = [
+  { id: '1', name: 'fauxkno', email: 'sup@fauxkno.com', age: 32 },
+  { id: '2', name: 'grafiq', email: 'graf@iq.com', age: 25 },
+  { id: '3', name: 'loro', email: 'loro@ipso.com', age: 99 }
+];
+
 module.exports = new GraphQLSchema({
   // something right here
 });
