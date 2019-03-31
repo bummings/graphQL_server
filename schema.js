01512f8c721e6ff7a965a6688cfbcc25386d3fa4
bummings <eabummings@gmail.com>
2019-03-31 18:48:57 -0400

    root query / resolver

--- schema.js
+++ schema.js
@@ -14,6 +14,33 @@ const customers = [
   { id: '3', name: 'loro', email: 'loro@ipso.com', age: 99 }
 ];
 
+// customer type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLString }
+  })
+});
+
+// Root query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  customer: {
+    type: CustomerType,
+    args: {
+      id: { GraphQLString }
+    },
+    resolve(parentValue, args) {
+      for (let i = 0; i < customers.length; i++) {
+        return customers[i];
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  // something right here
+  query: RootQuery
 });
